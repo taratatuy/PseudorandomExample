1f969f5d967269069e8ba52bd9c6a339ef4dbbd8
Alexandr Timofeev <taratatuy@yandex.ru>
2017-10-27 21:48:14 +0300

    Pseudorandom

---
+++ RandomTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                SomeClass SC = new SomeClass();
+                SomeMethod();
+                Console.ReadLine();
+            }
+        }
+
+        private static void SomeMethod()
+        {
+            Random random = new Random();
+
+            int roll = random.Next(100);
+
+            Console.WriteLine($"{roll}");
+        }
+    }
+}
---
+++ RandomTest/SomeClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomTest
+{
+    class SomeClass
+    {
+        public SomeClass()
+        {
+            SomeMethod();
+        }
+
+        private void SomeMethod()
+        {
+            Random random = new Random();
+
+            int roll = random.Next(100);
+
+            Console.WriteLine($"{roll}");
+        }
+    }
+}
